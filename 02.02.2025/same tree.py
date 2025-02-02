5cbef820187a801398045dfef6979dc71f38fee9
Kesavan879 <heshavanvr@gmail.com>
2025-02-02 17:53:47 +0530

    Create same tree.py

---
+++ 02.02.2025/same tree.py
@@ -0,0 +1,8 @@
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p==None and q==None:
+            return True
+        elif p==None or q==None or p.val!=q.val:
+            return False
+        return  self.isSameTree(p.left,q.left)  and self.isSameTree(p.right,q.right)
